c01dad63d02d6f546d3ebf09c37b915383033f0a
arian <kapqworks@gmail.com>
2022-08-22 13:22:19 +0430

    Update delete now removes paticipants

--- routes/deletePoll.js
+++ routes/deletePoll.js
@@ -1,26 +1,28 @@
 var express = require("express");
-var router = express.Router()
+var router = express.Router();
 const createSupabaseClient = require("@supabase/supabase-js").createClient;
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createSupabaseClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createSupabaseClient(supabaseUrl, supabaseKey);
 
 router.delete("/:id", async (req, res) => {
-
-    console.log(req.params.id);
-
     try {
+        const participanceData = await supabase
+            .from("participanceChoise")
+            .delete()
+            .eq('poll_id', req.params.id);
+
         const data = await supabase
             .from("polls")
             .delete()
-            .eq('id', req.params.id)
+            .eq('id', req.params.id);
 
-        res.send("deleted")
+        res.send("deleted");
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
 
 })
 
-module.exports = router
+module.exports = router;
